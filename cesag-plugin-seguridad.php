ae41b7c9236d1a7a34d3ed8ceae8df65e06d84af
Jesús Cortés <jesuscortes@artifices.net>
2018-10-17 11:33:01 +0200

    1.0.5 Mejoras updater
    Arreglado sistema de actualización por github, refactorización de funciones.

--- cesag-plugin-seguridad.php
+++ cesag-plugin-seguridad.php
@@ -3,60 +3,79 @@
 Plugin Name: Opciones de Seguridad para centros Pureza de Maria
 Plugin URI: https://www.artifices.net
 Description: Opciones básicas de seguridad: límite de revisiones, no actualizaciones automaticas, no edición de ficheros, no instalación de plugins. 
-* Version: 1.0.4
+* Version: 1.0.5
 * Author: Jesus Cortes
 * Author URI: http://www.artifices.net
 License: GPLv2 or later
 Text Domain: artifices
+GitHub Plugin URI: https://github.com/ArtificesGIT/cesag-seguridad-plugin
 */
 
-if( ! class_exists( 'Artifices_Updater' ) ){
+// Auto actualizador mediante Github
+
+if( ! class_exists( 'cesag_Updater' ) ){
     include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
 }
 
-$updater = new Artifices_Updater( __FILE__ );
-$updater->set_username( 'artifices' );
-$updater->set_repository( 'artifices-seguridad-plugin' );
+$updater = new cesag_Updater( __FILE__ );
+$updater->set_username( 'ArtificesGIT' );
+$updater->set_repository( 'cesag-seguridad-plugin' );
 /*
     $updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 */
 $updater->initialize();
 
 
-// LIMITAR NUMERO DE REVISIONES EN LA BD
+// Definición de configuraciones
 
-if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', 2);
-if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', false);
+function constants_cesag() {
 
-// IMPEDIR EDICION TEMAS & PLUGINS
+// Filtro para permitir solo al usuario artifices realizar tareas de mantenimiento.
 
-define('DISALLOW_FILE_EDIT',true);
+    $current_user = wp_get_current_user();
+    if ( 'cesag' != $current_user->user_login ) {
+        define('DISALLOW_FILE_EDIT',true); // IMPEDIR EDICION TEMAS & PLUGINS
+        define('DISALLOW_FILE_MODS',true); // IMPEDIR INSTALACION DE PLUGINS 
+    } else {
+        define('DISALLOW_FILE_EDIT',false); // PERMITIR A ARTIFICES EDICION TEMAS & PLUGINS
+        define('DISALLOW_FILE_MODS',false); // PERMITIR A ARTIFICES INSTALACION DE PLUGINS    
+    }
 
-// IMPEDIR INSTALACION DE PLUGINS
+    // LIMITAR NUMERO DE REVISIONES EN LA BD
 
-define('DISALLOW_FILE_MODS',true);
+    if (!defined('WP_POST_REVISIONS')) {
+        define('WP_POST_REVISIONS', 2);
+    }
+    if (!defined('WP_POST_REVISIONS')) {
+        define('WP_POST_REVISIONS', false);
+    }
 
-// Limitar subidas
+    // Limitar subidas
 
- @ini_set( 'upload_max_size' , '5M' );
+     @ini_set( 'upload_max_size' , '5M' );
+
+}
+add_action('init', 'constants_cesag', 102);
+
 
 // PERSONALIZAR LOGO LOGIN
 
-function my_login_logo() { ?>
+function my_login_logo_style() { ?>
     <style type="text/css">
-        #login h1 a, .login h1 a {
-        background-image: url('<?php echo plugins_url( 'logo-pureza.png', __FILE__ ) ; ?>');
-		height:137px;
-		width:110px;
-		background-size: 110px 137px;
-		background-repeat: no-repeat;
-        	padding-bottom: 30px;
+        #login h1 a, 
+        .login h1 a {
+            background-image: url('<?php echo plugins_url( 'logo-artifices.png', __FILE__ ) ; ?>') !important;
+            height:52px;
+            width:240px;
+            background-size: 240px 52px;
+            background-repeat: no-repeat;
+            padding-bottom: 30px;
         }
     </style>
 <?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
-add_filter( 'login_headerurl', 'custom_loginlogo_url' );
+add_action('login_enqueue_scripts', 'my_login_logo_style' );
+add_filter('login_headerurl', 'custom_loginlogo_url' );
 function custom_loginlogo_url($url) {
-    return 'http://pmaria.es';
+    return 'https://www.artifices.net';
 }
 ?>
